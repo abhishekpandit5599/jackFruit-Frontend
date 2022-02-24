4c620da15e4d6a2554553687afed09c2f8bfc620
Abhishek Sharma <abhishekpandit5599@gmail.com>
2022-02-24 22:32:47 +0530

    Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,30 +23,7 @@ function Home() {
   const [bas50, setBas50] = useState(0);
   const [rent10OfBas, setRent10OfBas] = useState(0);
 
-  const metroCities = [
-    "ahmedabad",
-    "bangalore",
-    " chennai",
-    "coimbatore",
-    "gorakhpur",
-    "hyderabad",
-    "jaipur",
-    "jodhpur",
-    "kanpur",
-    "kochi",
-    "kolkata",
-    "kozhikode",
-    "madurai",
-    "mumbai",
-    "nagpur",
-    "national capital region",
-    "patna",
-    "pune",
-    "raipur",
-    "salem",
-    "surat",
-    "thiruvananthapuram and visakhapatnam",
-  ];
+    
 
   useEffect(() => {
     if(!localStorage.getItem("auth-token")){
@@ -55,36 +32,45 @@ function Home() {
     // eslint-disable-next-line
   }, []);
 
+
+  // Handle the onClick of calculate button
   const onClickHandler = () => {
     document.getElementById("pop-up").style.display = "block";
     document.getElementById("main-input").style.filter = "blur(2px)";
     checkMetroCity();
   };
 
+
+  // Check city is Metro or Non Metro
   const checkMetroCity = () => {
-    if (metroCities.includes(city.toLowerCase())) {
+    if (city==='Metro') {
       appHRA50();
     } else {
       appHRA40();
     }
   };
 
+  // Calculate the Metro city
   const appHRA50 = () => {
-    setBas50((bas * 12 * 50) / 100);
-    setRent10OfBas(rent * 12 - (bas * 12 * 10) / 100);
-    const bas50Reasult = Math.min(bas50, rent10OfBas, hra * 12);
+    setBas50((bas * 50) / 100);
+    setRent10OfBas(rent - ((bas * 10) / 100));
+    const bas50Reasult = Math.min(bas50, rent10OfBas, hra);
     setAppHra(bas50Reasult);
   };
 
+
+  // Calculate the Non Metro city
   const appHRA40 = () => {
-    setBas50((bas * 12 * 40) / 100);
-    setRent10OfBas(rent * 12 - (bas * 12 * 10) / 100);
-    const bas50Reasult = Math.min(bas50, rent10OfBas, hra * 12);
+    setBas50((bas * 40) / 100);
+    setRent10OfBas(rent - ((bas * 10) / 100));
+    const bas50Reasult = Math.min(bas50, rent10OfBas, hra);
     setAppHra(bas50Reasult);
   };
 
+
+  // Handle after confirm and also call api and save the data in the database
   const confirmSubmitHandler = async () => {
-    const taxIncResult = ((Number(bas) + Number(lta) + Number(hra) + Number(fa)) * 12) -appHra -(inv * 12) -(med * 12);
+    const taxIncResult = ((Number(bas) + Number(lta) + Number(hra) + Number(fa))) -appHra -inv -med;
     setTaxInc(taxIncResult);
     try {
       const requestOptions = {
@@ -94,14 +80,14 @@ function Home() {
           "auth-token": localStorage.getItem("auth-token"),
         },
         body: JSON.stringify({
-          bas: bas*12,
-          lta: lta*12,
-          hra: hra*12,
-          fa: fa*12,
-          inv: inv*12,
-          rent: rent*12,
+          bas: bas,
+          lta: lta,
+          hra: hra,
+          fa: fa,
+          inv: inv,
+          rent: rent,
           cityType: city,
-          med: med*12,
+          med: med,
           appHra: appHra,
           taxInc: taxIncResult,
         }),
@@ -116,10 +102,13 @@ function Home() {
     finalPopUp();
   };
 
+
+  // Handle the final pop up - Tax Inc
   const finalPopUp=()=>{
       document.getElementById('pop-up-taxInc').style.display= "block";
   }
 
+  // Handle the close button of popup
   const handleCrossBtn=()=>{
     document.getElementById('pop-up-taxInc').style.display= "none";
     document.getElementById("main-input").style.filter = "blur(0px)";
@@ -132,6 +121,7 @@ function Home() {
         <div className="first-input-container input-container">
           <form>
             <input
+              className="display-block-class"
               onChange={(e) => setBas(e.target.value)}
               name="bas"
               value={bas}
@@ -140,6 +130,7 @@ function Home() {
             />
             <input
               onChange={(e) => setLta(e.target.value)}
+              className="display-block-class"
               name="lta"
               value={lta}
               placeholder="LTA"
@@ -147,6 +138,7 @@ function Home() {
             />
             <input
               onChange={(e) => setHra(e.target.value)}
+              className="display-block-class"
               name="hra"
               value={hra}
               placeholder="HRA"
@@ -154,6 +146,7 @@ function Home() {
             />
             <input
               onChange={(e) => setFa(e.target.value)}
+              className="display-block-class"
               name="fa"
               value={fa}
               placeholder="FA"
@@ -165,6 +158,7 @@ function Home() {
           <form>
             <input
               onChange={(e) => setInv(e.target.value)}
+              className="display-block-class"
               name="inv"
               value={inv}
               placeholder="Inv"
@@ -172,20 +166,20 @@ function Home() {
             />
             <input
               onChange={(e) => setRent(e.target.value)}
+              className="display-block-class"
               name="rent"
               value={rent}
               placeholder="Rent"
               type="number"
             />
-            <input
-              onChange={(e) => setCity(e.target.value)}
-              name="city"
-              value={city}
-              placeholder="CityType"
-              type="text"
-            />
+            <div className="cityTypeLabel">
+              City Type : 
+              <input onChange={(e)=>setCity(e.target.value)} className="cityType" type="radio" value="Metro" name="cityType" /> Metro
+              <input onChange={(e)=>setCity(e.target.value)} className="cityType" type="radio" value="Non metro" name="cityType" /> Non metro
+            </div>
             <input
               onChange={(e) => setMed(e.target.value)}
+              className="display-block-class"
               name="med"
               value={med}
               placeholder="Med"
@@ -217,14 +211,14 @@ function Home() {
           <table>
             <tr>
               <th>Actual HRA :</th>
-              <td>{hra*12}</td>
+              <td>{hra}</td>
             </tr>
             <tr>
               <th>Actual Rent - 10% BAS :</th>
               <td>{rent10OfBas}</td>
             </tr>
             <tr>
-              <th>50%/40% of BAS  :</th>
+              <th>{city==='Metro'?'50% of BAS : ': '40% of BAS : '}</th>
               <td>{bas50}</td>
             </tr>
             <tr>
